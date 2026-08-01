ff32f1954cdc400da4a3e29110862c50a7e178fa
Flowboyz <flowboyzamy@gmail.com>
2026-08-01 23:49:56 +0100

    lotterry telegram

--- config.py
+++ config.py
@@ -3,8 +3,10 @@ Ditto Dinky - Configuration
 """
 import os
 from datetime import timedelta
+from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, ".env"))
 
 
 class Config:
